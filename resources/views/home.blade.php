d17e2c9c3ed9d3e9798d31825e235ee31d867c27
musmula <oliver.dvorski@gmail.com>
2017-02-18 13:14:46 +0100

    Install Bulma and start with some basic configuration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <link rel="stylesheet" href="{{ url('/css/app.css') }}">
+        <title>{{ config('app.name') }}</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Hello</h1>
+        </div>
+    </body>
+</html>
