026ba20c5eb90555192ed74a88f23c81da824e8a
musmula <oliver.dvorski@gmail.com>
2017-02-25 19:45:17 +0100

    Remove column class from download view

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -4,40 +4,38 @@
     @include('partials.standardHeader')
 
     <div class="container">
-        <div class="column is-4 is-offset-4">
-            <div class="section has-text-centered">
-                <h1 class="title is-2">Your archive is ready!</h1>
-                <p>
-                    <qr url="{{ url($fileUrl) }}"></qr>
-                </p>
-                <p class="control has-addons has-text-centered has-flex-centered">
-                    <input type="text" disabled class="input is-large has-addons" value="{{ url($fileUrl) }}">
-                    <a @click.prevent="copy('{{ url($fileUrl) }}')" class="button is-large" title="Copy to clipboard">
-                        <span class="icon">
-                            <i class="fa fa-clipboard"></i>
-                        </span>
-                    </a>
-                </p>
-                <br>
-                <a href="{{ url('/' . $fileUrl . '/download') }}" class="button is-medium is-primary"
-                    @if ($hasPassword)
-                        @click.prevent="showPasswordModal = true"
-                    @endif
-                    title="Download archive" 
-                >
-                    <span class="icon is-small">
-                        <span class="fa fa-download"></span>
+        <div class="section has-text-centered">
+            <h1 class="title is-2">Your archive is ready!</h1>
+            <p>
+                <qr url="{{ url($fileUrl) }}"></qr>
+            </p>
+            <p class="control has-addons has-text-centered has-flex-centered">
+                <input type="text" disabled class="input is-large has-addons" value="{{ url($fileUrl) }}">
+                <a @click.prevent="copy('{{ url($fileUrl) }}')" class="button is-large" title="Copy to clipboard">
+                    <span class="icon">
+                        <i class="fa fa-clipboard"></i>
                     </span>
-                    <span>Download</span>
                 </a>
+            </p>
+            <br>
+            <a href="{{ url('/' . $fileUrl . '/download') }}" class="button is-medium is-primary"
+                @if ($hasPassword)
+                    @click.prevent="showPasswordModal = true"
+                @endif
+                title="Download archive" 
+            >
+                <span class="icon is-small">
+                    <span class="fa fa-download"></span>
+                </span>
+                <span>Download</span>
+            </a>
 
-                <a href="{{ url('/') }}" class="button is-medium" title="Upload new files to a new archive">
-                    <span class="icon is-small">
-                        <span class="fa fa-upload"></span>
-                    </span>
-                    <span>Upload</span>
-                </a>
-            </div>
+            <a href="{{ url('/') }}" class="button is-medium" title="Upload new files to a new archive">
+                <span class="icon is-small">
+                    <span class="fa fa-upload"></span>
+                </span>
+                <span>Upload</span>
+            </a>
         </div>
     </div>
 
