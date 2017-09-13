91e67f13733f64f74177870597a7c2bbe8d58793
Musmula <oliver.dvorski@gmail.com>
2017-09-13 16:33:53 +0200

    Add password protection to notes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/about', 'PageController@about');
 Route::get('/note', 'NoteController@form')->name('note');
 Route::post('/note', 'NoteController@store')->name('note.store');
 Route::get('/note/{note}', 'NoteController@show')->name('note.show');
+Route::post('/note/{note}/check', 'NoteController@checkPassword')->name('note.checkPassword');
 
 Route::get('/{url}', 'FileController@downloadPage');
 Route::get('/{url}/download', 'FileController@download');
