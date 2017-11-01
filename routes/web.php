5af65dfc0a3bcd89aecdfffb184a056f8b90e6ea
Musmula <oliver.dvorski@gmail.com>
2017-11-01 21:46:27 +0100

    Remove note sharing support

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,6 @@
 Route::get('/', 'PageController@home');
 Route::get('/about', 'PageController@about');
 
-Route::get('/note', 'NoteController@form')->name('note');
-Route::post('/note', 'NoteController@store')->name('note.store');
-Route::get('/note/{note}', 'NoteController@show')->name('note.show');
-Route::post('/note/{note}/check', 'NoteController@checkPassword')->name('note.checkPassword');
-
 Route::get('/{url}', 'FileController@downloadPage');
 Route::get('/{url}/download', 'FileController@download');
 Route::post('/{url}/download', 'FileController@download');
